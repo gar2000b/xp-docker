a82124ae15ad2e200596ab339b0f557e3522dd7c
Popeye <gar2000b@yahoo.com>
2025-12-09 22:05:54 +0000

    Updated schema with dates

--- sql/xp-users/data/insert/01_insert_users.sql
+++ sql/xp-users/data/insert/01_insert_users.sql
@@ -1,12 +1,12 @@
-INSERT INTO users (name) VALUES
-('Alice Wonder'),
-('Bob Stone'),
-('Charlie Sparks'),
-('Diana Rivers'),
-('Evan Cloud'),
-('Fiona Blaze'),
-('Gavin Steel'),
-('Hazel Birch'),
-('Ivan Night'),
-('Jade Frost');
+INSERT INTO users (username, first_name, last_name, email) VALUES
+('awonder', 'Alice', 'Wonder', 'alice.wonder@example.com'),
+('bstone', 'Bob', 'Stone', 'bob.stone@example.com'),
+('csparks', 'Charlie', 'Sparks', 'charlie.sparks@example.com'),
+('drivers', 'Diana', 'Rivers', 'diana.rivers@example.com'),
+('ecloud', 'Evan', 'Cloud', 'evan.cloud@example.com'),
+('fblaze', 'Fiona', 'Blaze', 'fiona.blaze@example.com'),
+('gsteel', 'Gavin', 'Steel', 'gavin.steel@example.com'),
+('hbirch', 'Hazel', 'Birch', 'hazel.birch@example.com'),
+('inight', 'Ivan', 'Night', 'ivan.night@example.com'),
+('jfrost', 'Jade', 'Frost', 'jade.frost@example.com');
 
