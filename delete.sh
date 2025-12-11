fcfad6b5b05cb7c4cf81b4f6a63b2996ec15657b
Gary Black <gar2000b@yahoo.com>
2025-12-11 14:59:54 -0500

    Updating delete

--- delete.sh
+++ delete.sh
@@ -6,8 +6,10 @@ cd "$(dirname "$0")"
 echo ">>> Stopping containers..."
 docker compose down
 
-echo ">>> Removing MariaDB volume docker_mariadb_data..."
-docker volume rm xp-docker_mariadb_data || echo "Volume docker_mariadb_data not found, skipping."
+echo ">>> Removing volumes..."
+docker volume rm xp-docker_mariadb_data || echo "Volume xp-docker_mariadb_data not found, skipping."
+docker volume rm xp-docker_influxdb_data || echo "Volume xp-docker_influxdb_data not found, skipping."
+docker volume rm xp-docker_influxdb_config || echo "Volume xp-docker_influxdb_config not found, skipping."
 
 echo ">>> Delete complete."
 
